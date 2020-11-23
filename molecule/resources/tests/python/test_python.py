377f3369c96cc74b81637419a0a43a8eb9e7cbae
Nicola Musatti <nicola.musatti@gmail.com>
2020-11-23 11:02:11 +0100

    Python test-related corrections

--- molecule/resources/tests/python/test_python.py
+++ molecule/resources/tests/python/test_python.py
@@ -23,7 +23,7 @@ def test_python2(host):
     host.check_output('/usr/bin/python2 --version').find('2') > -1
 
 
-def test_source_python(host):
+def test_source_python3(host):
     f = host.file('/opt/Python-3.7/bin/python3.7')
     assert f.exists
     assert f.user == 'python'
@@ -31,3 +31,13 @@ def test_source_python(host):
 
     host.check_output('/opt/Python-3.7/bin/python3.7 --version').find(
             '3.7.9') > -1
+
+
+def test_source_python2(host):
+    f = host.file('/opt/Python-2.7/bin/python2.7')
+    assert f.exists
+    assert f.user == 'python'
+    assert f.group == 'python'
+
+    host.check_output('/opt/Python-2.7/bin/python2.7 --version').find(
+            '2.7.18') > -1
