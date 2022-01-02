246315ea03dd4399cc007abf2ef1112d0c522068
Nicola Musatti <nicola.musatti@gmail.com>
2022-01-02 12:51:16 +0100

    Add preliminary Ubuntu LTS support

--- molecule/resources/tests/complete/test_complete.py
+++ molecule/resources/tests/complete/test_complete.py
@@ -7,26 +7,30 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
+def is_ubuntu(host):
+    res = host.run('grep Ubuntu /etc/os-release')
+    return res.rc == 0
+
+
 def test_system_package(host):
     f = host.file('/usr/bin/gvim')
-
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
 
 
 def test_flatpak_system(host):
-    f = host.file('/var/lib/flatpak/app/org.musicbrainz.Picard')
-
-    assert f.is_directory
-    assert f.user == 'root'
-    assert f.group == 'root'
+    if not is_ubuntu(host):
+        f = host.file('/var/lib/flatpak/app/org.musicbrainz.Picard')
+        assert f.is_directory
+        assert f.user == 'root'
+        assert f.group == 'root'
 
 
 def test_flatpak_user(host):
-    f = host.file('/home/user/.local/share/flatpak/app/'
-                  'io.github.quodlibet.QuodLibet')
-
-    assert f.is_directory
-    assert f.user == 'user'
-    assert f.group == 'user'
+    if not is_ubuntu(host):
+        f = host.file('/home/user/.local/share/flatpak/app/'
+                      'io.github.quodlibet.QuodLibet')
+        assert f.is_directory
+        assert f.user == 'user'
+        assert f.group == 'user'
